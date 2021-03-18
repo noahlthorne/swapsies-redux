2867c3b36eb9aa71921312d0572b910da961b267
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2021-03-18 18:46:59 +0900

    add title column

--- server/database.sql
+++ server/database.sql
@@ -2,9 +2,10 @@ CREATE DATABASE swapsies;
 
 CREATE TABLE games(
   game_id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
   console VARCHAR(255),
   description TEXT,
   cover_image TEXT,
   rating FLOAT(5),
   release_date DATE
-)
+);
