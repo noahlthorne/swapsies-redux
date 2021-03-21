670b7aeb2b5da67efb6389c42191c53d3bdf6a23
Noah Thorne <noahlthorne@gmail.com>
2021-03-21 11:06:48 +0900

    connect to db when launching app

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Connect to database
+connectDB();
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 app.get("/", (req, res) => {
