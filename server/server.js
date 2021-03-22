ba6257ebca8258844565756a2ba6cb17c5e95940
Noah Thorne <noahlthorne@gmail.com>
2021-03-22 16:51:21 +0900

    add routes to server.js

--- server/server.js
+++ server/server.js
@@ -13,3 +13,11 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 app.get("/", (req, res) => {
 	res.send("API Running");
 });
+
+// Define Routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/games", require("./routes/api/games"));
+app.use("/api/listings", require("./routes/api/listings"));
+app.use("/api/swaps", require("./routes/api/swaps"));
